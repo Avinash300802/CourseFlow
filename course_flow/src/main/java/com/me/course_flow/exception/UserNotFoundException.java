6611c53ec76d31bdfe211ba482f3d967a9415899
Avinash R C <70832614+Avinash300802@users.noreply.github.com>
2023-08-01 01:51:45 +0530

    Delete course_flow directory

--- course_flow/src/main/java/com/me/course_flow/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.me.course_flow.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not found the user with id "+ id);
-    }
-}
--- course_flow/src/main/java/com/me/course_flow/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.me.course_flow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.me.course_flow.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
